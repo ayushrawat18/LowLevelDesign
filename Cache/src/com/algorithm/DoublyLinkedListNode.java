68b73ae065094109967107ad78ded2b86ccea346
ayush.rawat <ayush.rawat@servicenow.com>
2022-02-22 21:27:24 +0530

    Design of doubly linked list

---
+++ Cache/src/com/algorithm/DoublyLinkedListNode.java
@@ -0,0 +1,16 @@
+package com.algorithm;
+
+public class DoublyLinkedListNode {
+
+    Integer data;
+    DoublyLinkedListNode left;
+    DoublyLinkedListNode right;
+
+    public DoublyLinkedListNode(Integer data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+
+}
